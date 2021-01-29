87cae9495ee20b942e33e7509f6b581deba5d6e9
Bharath-pg <bharathpg1997@gmail.com>
2021-01-29 20:24:59 +0530

    No changes

--- bankapi/urls.py
+++ bankapi/urls.py
@@ -5,7 +5,4 @@ from .views import HomePageView, SearchResultsView
 urlpatterns = [
     path('search/', SearchResultsView.as_view(), name='search_results'),
     path('', HomePageView.as_view(), name='home'),
-   
-    
-
 ]
